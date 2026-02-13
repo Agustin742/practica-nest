233985fd8ee7544f2e73270dc0e266ab38b67ae6
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-02-13 18:36:58 -0300

    Uso de Http status

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import type { Request, Response } from 'express';
 
 @Controller()
@@ -9,5 +9,23 @@ export class HelloController {
     response.status(200).json({
       message: 'hello world',
     });
+  }
+
+  @Get('new')
+  @HttpCode(201)
+  somethingNew() {
+    return 'something New';
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFoundPage() {
+    return '404 not found';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  errorPage() {
+    return 'Error route';
   }
 }
