bc33e20a322bdb97e8360e8ef54a2e290045df18
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-02-13 13:50:29 -0300

    Entendiendo el uso de los metodos http

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TaskService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TaskController {
   taskService: TaskService;
 
@@ -9,8 +9,28 @@ export class TaskController {
     this.taskService = taskService;
   }
 
-  @Get('/tasks')
+  @Get()
   getAllTasks() {
     return this.taskService.getTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.taskService.createTask();
+  }
+
+  @Put()
+  updateTask() {
+    return this.taskService.updateTask();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.taskService.deleteTask();
+  }
+
+  @Patch()
+  updateTaskStatus() {
+    return this.taskService.updateTaskStatus();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TaskService {
   getTasks() {
     return ['task 1', 'task 2', 'task 3'];
   }
+
+  createTask() {
+    return 'Creando tarea';
+  }
+
+  updateTask() {
+    return 'Actualizando tarea';
+  }
+
+  deleteTask() {
+    return 'Borrado tarea';
+  }
+
+  updateTaskStatus() {
+    return 'Actualizando estado de la tarea';
+  }
 }
